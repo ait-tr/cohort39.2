10aed10e4b9487b3cb251bbddbdf09a412880980
StanislavPB <perminovsb@gmail.com>
2023-11-08 19:22:03 +0000

    add lesson 2

---
+++ basic_programming/lesson_02/code/lessoncode/ArithmeticOperations.java
@@ -0,0 +1,5 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        System.out.println("2 + 2 = " + 4);
+    }
+}
---
+++ basic_programming/lesson_02/code/lessoncode/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    // класс - это программа которую мы хотим
+    // чтобы java-машина выполнила
+
+    public static void main(String[] args) {
+
+     // main - это стандартный метод который java-машина
+     // понимает как точку входа (запуска) нашей программы
+
+        System.out.println("Hello World!");
+
+
+    }
+}
