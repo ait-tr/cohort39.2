1097f7272d962a0a2591a1f593a2bd87e5a6edfe
StanislavPB <perminovsb@gmail.com>
2023-11-13 19:59:03 +0000

    add lesson 4

---
+++ basic_programming/lesson_04/code/lessoncode/car/Car.java
@@ -0,0 +1,42 @@
+package car;
+
+public class Car {
+
+    /*
+    1. Создайте класс для описания автомобиля
+у автомобиля должны быть следующие характеристики:
+- model
+- объем двигателя
+- цвет
+- трансмиссия ручная или автомат
+
+2. Автомобиль должен уметь:
+- сигналить
+- начинать движение
+- останавливаться
+     */
+
+    String model;
+    double engine;
+    String color;
+    boolean transmissionAuto;
+
+    public Car(String model, double engine, String color, boolean transmissionAuto) {
+        this.model = model;
+        this.engine = engine;
+        this.color = color;
+        this.transmissionAuto = transmissionAuto;
+    }
+
+    public void beep(){
+        System.out.println("This is " + model + "! BEEP!");
+    }
+
+    public void start(){
+        String textMessage = "Start engine! ";
+        System.out.println(textMessage + engine);
+    }
+
+    public void stop(){}
+
+}
---
+++ basic_programming/lesson_04/code/lessoncode/car/CarDemo.java
@@ -0,0 +1,14 @@
+package car;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mercedes", 2.5, "black", true );
+
+        System.out.println(car1.model);
+        car1.beep();
+
+        Car car2 = new Car("Honda", 3.0, "silver", false);
+
+
+    }
+}
