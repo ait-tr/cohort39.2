9eafae4d0e703ea6c2df2af3a761b4172230e990
StanislavPB <perminovsb@gmail.com>
2023-11-29 21:02:48 +0000

    add lesson 11 (search and sort array)

---
+++ basic_programming/lesson_11/code/lessoncode/StringCompare.java
@@ -0,0 +1,10 @@
+public class StringCompare {
+    public static void main(String[] args) {
+        // compareTo
+        // "ABCDEFG" сравнить с "ABCEFGD"
+
+        char literal1 = '1';
+
+        System.out.println((int) literal1);
+    }
+}
