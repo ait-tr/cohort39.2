a548a3c02172758ff07dbc24f1f9431b948616ea
StanislavPB <perminovsb@gmail.com>
2023-12-04 20:10:00 +0000

    add lesson 13

---
+++ basic_programming/lesson_13/code/lessoncode/school/SchoolApp2.java
@@ -0,0 +1,17 @@
+package school;
+
+import java.util.Arrays;
+
+public class SchoolApp2 {
+    public static void main(String[] args) {
+
+        Student studentsElena = new Student("Elena", 1);
+        Student studentsValeria = new Student("Valeria", 1);
+        Student studentsOtalbec = new Student("Otalbec", 3);
+
+        Student[] students = new Student[3];
+
+        students[0] = new Student("Elena", 1);
+
+    }
+}
