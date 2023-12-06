5e1ae3cf8256c73d544e47e8d976e049a3971525
StanislavPB <perminovsb@gmail.com>
2023-12-06 18:58:24 +0000

    add lesson 14

--- basic_programming/lesson_14/code/lessoncode/setStore/entity/SetToolsRepository.java
+++ basic_programming/lesson_14/code/lessoncode/setStore/entity/SetToolsRepository.java
@@ -4,26 +4,26 @@ import java.util.Arrays;
 
 public class SetToolsRepository {
 
-    SetTool[] setTools = new SetTool[3];
+    SetTool[] setToolsRepo = new SetTool[3];
 
     public SetToolsRepository(SetTool setTools1, SetTool setTools2, SetTool setTools3) {
-        setTools[0] = setTools1;
-        setTools[1] = setTools2;
-        setTools[2] = setTools3;
+        setToolsRepo[0] = setTools1;
+        setToolsRepo[1] = setTools2;
+        setToolsRepo[2] = setTools3;
     }
 
     public SetTool[] getSetTools() {
-        return setTools;
+        return setToolsRepo;
     }
 
     public void setSetTools(SetTool[] setTools) {
-        this.setTools = setTools;
+        this.setToolsRepo = setTools;
     }
 
     @Override
     public String toString() {
         return "SetToolsRepository{" +
-                "setTools=" + Arrays.toString(setTools) +
+                "setTools=" + Arrays.toString(setToolsRepo) +
                 '}';
     }
 }
