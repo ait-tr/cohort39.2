ad38a46a69e2a4f3627745a93e4d0f7daf9ede8a
StanislavPB <perminovsb@gmail.com>
2024-01-12 18:58:54 +0000

    add lesson 24

---
+++ basic_programming/lesson_24/code/lessoncode/collections/list/DifferentMethodDemo.java
@@ -0,0 +1,12 @@
+package collections.list;
+
+import java.util.ArrayList;
+
+public class DifferentMethodDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.trimToSize();
+
+    }
+}
---
+++ basic_programming/lesson_24/code/lessoncode/collections/map/mapDemo/HashMapDemo.java
@@ -0,0 +1,7 @@
+package collections.map.mapDemo;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+
+    }
+}
