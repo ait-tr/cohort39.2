2617780136d97213c0207a027f553137fc72d138
StanislavPB <perminovsb@gmail.com>
2024-01-12 20:01:25 +0000

    add lesson 24

---
+++ basic_programming/lesson_24/code/lessoncode/collections/set/HashSetExample.java
@@ -0,0 +1,26 @@
+package collections.set;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> mySet = new HashSet<>();
+
+        mySet.add("A");
+        mySet.add("A");
+        mySet.add("B");
+        mySet.add("A");
+        mySet.add("B");
+        mySet.add("C");
+        mySet.add("D");
+        mySet.add("A");
+        mySet.add("D");
+        mySet.add("E");
+        mySet.add("A");
+        mySet.add("F");
+
+        System.out.println(mySet);
+    }
+}
