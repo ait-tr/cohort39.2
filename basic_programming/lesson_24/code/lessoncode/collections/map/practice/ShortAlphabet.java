e6bbd510e6476c32f669430b9fe89e5369d9dd45
StanislavPB <perminovsb@gmail.com>
2024-01-12 19:22:02 +0000

    add lesson 24

---
+++ basic_programming/lesson_24/code/lessoncode/collections/map/practice/ShortAlphabet.java
@@ -0,0 +1,10 @@
+package collections.map.practice;
+
+public class ShortAlphabet {
+    public static void main(String[] args) {
+        /*
+        Создать коллекцию HashMap где в качестве ключа - буква, значение - слово на эту букву.
+        Добавить 10 элементов и проверить содержимое коллекции
+         */
+    }
+}
