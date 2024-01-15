3d0bd8b4181af44e26ffdd7d12f8f6d1180bdf10
StanislavPB <perminovsb@gmail.com>
2024-01-15 19:16:34 +0000

    add lesson 25

---
+++ basic_programming/lesson_25/code/lessoncode/practice/CombinedComparator.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Comparator;
+
+public class CombinedComparator {
+    Comparator<Suitcase> bySize = new ComparatorSize();
+    Comparator<Suitcase> byMaterial = new ComparatorMaterial();
+    Comparator<Suitcase> byColor = new ComparatorColor();
+
+    Comparator<Suitcase> combined = bySize.thenComparing(byMaterial.thenComparing(byColor));
+
+    public Comparator<Suitcase> getCombined() {
+        return combined;
+    }
+}
