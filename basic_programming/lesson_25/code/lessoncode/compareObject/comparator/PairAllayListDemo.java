2fcf51971306edbcc68d5c55eb09ac31bdbac007
StanislavPB <perminovsb@gmail.com>
2024-01-15 18:23:21 +0000

    add lesson 25

---
+++ basic_programming/lesson_25/code/lessoncode/compareObject/comparator/PairAllayListDemo.java
@@ -0,0 +1,12 @@
+package compareObject.comparator;
+
+import java.util.ArrayList;
+
+public class PairAllayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Pair> pairs = new ArrayList<>();
+        pairs.add(new Pair("First", 1));
+
+        System.out.println(pairs);
+    }
+}
