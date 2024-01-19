4fa764d460a26a983c63455631f32b916d038528
StanislavPB <perminovsb@gmail.com>
2024-01-19 20:42:38 +0000

    add lesson 27

---
+++ basic_programming/lesson_27/code/lessoncode/practice/library/Book.java
@@ -0,0 +1,49 @@
+package practice.library;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(genre, book.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, genre);
+    }
+}
