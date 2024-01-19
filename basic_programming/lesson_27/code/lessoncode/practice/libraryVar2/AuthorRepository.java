a82ef7b019607530dbfc9716289f752c7b97d7db
StanislavPB <perminovsb@gmail.com>
2024-01-19 23:18:07 +0000

    add lesson 27

--- basic_programming/lesson_27/code/lessoncode/practice/libraryVar2/AuthorRepository.java
+++ basic_programming/lesson_27/code/lessoncode/practice/libraryVar2/AuthorRepository.java
@@ -3,33 +3,27 @@ package practice.libraryVar2;
 import java.util.*;
 
 public class AuthorRepository {
-    private Set<String> authors = new HashSet<>();
-    public void addAuthor(String author) {
-        authors.add(author);
+    private Map<String, Set<Book>> authorsBooks = new HashMap<>();
+
+    public void addAuthor(Book book) {
+        String author = book.getAuthor();
+        if (!authorsBooks.containsKey(author)) {
+            authorsBooks.put(author, new HashSet<>());
+        }
+        authorsBooks.get(author).add(book);
     }
 
-    public Set<String> getAuthors(){
-        return authors;
+    public Set<Book> getBooksByAuthor(String author) {
+        return authorsBooks.getOrDefault(author, new HashSet<>());
     }
 
-    public void removeAuthorIfNotFoundBooks(String author, BookRepository bookRepository){
-        Iterator<String> iterator = authors.iterator();
-
-        while (iterator.hasNext()){
-            String currentAuthor = iterator.next();
-            if (currentAuthor.equals(author)) {
-                boolean hasBook = false;
-                for (Book book : bookRepository.getBooks()){
-                    if (book.getAuthor().equals(author)){
-                        hasBook = true;
-                        break;
-                    }
-                }
-                if (!hasBook) {
-                    iterator.remove();
-                }
+    public void removeBook(Book book) {
+        String author = book.getAuthor();
+        if (authorsBooks.containsKey(author)) {
+            authorsBooks.get(author).remove(book);
+            if (authorsBooks.get(author).isEmpty()) {
+                authorsBooks.remove(author);
             }
         }
     }
-
 }
