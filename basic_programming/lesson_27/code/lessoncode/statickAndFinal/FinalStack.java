c445f81b71ae80471e03ee1df76bc54f49cbe47f
StanislavPB <perminovsb@gmail.com>
2024-01-19 19:39:34 +0000

    add lesson 27

---
+++ basic_programming/lesson_27/code/lessoncode/statickAndFinal/FinalStack.java
@@ -0,0 +1,14 @@
+package statickAndFinal;
+
+import java.util.List;
+
+public class FinalStack {
+
+    final List<String> stringStack;
+    final String finalString;
+
+    public FinalStack(List<String> stringStack, String finalString) {
+        this.stringStack = stringStack;
+        this.finalString = finalString;
+    }
+}
