ffa3fbfc1df588995206525e9511edcdd9bd2e75
StanislavPB <perminovsb@gmail.com>
2024-01-22 20:28:42 +0000

    add lesson 28

---
+++ basic_programming/lesson_28/code/lessoncode/shoppinglist/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package shoppinglist.repository;
+
+import shoppinglist.dto.ProductDto;
+import shoppinglist.entity.Product;
+
+import java.util.List;
+
+public interface ProductRepository {
+
+    Integer addProduct(ProductDto productDto);
+    List<Product> findAll();
+    Product findById(Integer id);
+}
---
+++ basic_programming/lesson_28/code/lessoncode/shoppinglist/repository/ProductRepositoryArrayList.java
@@ -0,0 +1,43 @@
+package shoppinglist.repository;
+
+import shoppinglist.dto.ProductDto;
+import shoppinglist.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepositoryArrayList implements ProductRepository{
+
+    private List<Product> database;
+
+    private Integer idCounter;
+
+    public ProductRepositoryArrayList() {
+        this.database = new ArrayList<>();
+        this.idCounter = 0;
+    }
+
+    @Override
+    public Integer addProduct(ProductDto productDto) {
+        idCounter++;
+        database.add(new Product(idCounter, productDto.getProductName(), productDto.getPrice(), productDto.getDescription()));
+        return idCounter;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return database;
+    }
+
+    @Override
+    public Product findById(Integer id) {
+
+        for (Product currentProduct : database) {
+            if (currentProduct.getId().equals(id)) {
+                return currentProduct;
+            }
+        }
+
+        return null;
+    }
+}
