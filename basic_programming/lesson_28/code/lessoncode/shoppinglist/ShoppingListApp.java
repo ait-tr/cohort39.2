d2b71f111b83d0e1baf422d31c7d421072d3f924
StanislavPB <perminovsb@gmail.com>
2024-01-22 20:56:20 +0000

    add lesson 28

---
+++ basic_programming/lesson_28/code/lessoncode/shoppinglist/ShoppingListApp.java
@@ -0,0 +1,36 @@
+package shoppinglist;
+
+import shoppinglist.repository.ProductRepository;
+import shoppinglist.repository.ProductRepositoryArrayList;
+import shoppinglist.service.ProductService;
+import shoppinglist.service.Validation;
+import shoppinglist.ui.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingListApp {
+    public static void main(String[] args) {
+// создаем основные классы для работы сервиса
+
+        ProductRepository repository = new ProductRepositoryArrayList();
+        Validation validation = new Validation();
+        ProductService service = new ProductService(repository, validation);
+
+
+        // создаем классы для формирования пунктов меню
+
+        AddProductMenu addProductMenu = new AddProductMenu(service);
+        FindAllProductMenu findAllProductMenu = new FindAllProductMenu(service);
+        ExitMenu exitMenu = new ExitMenu();
+
+        List<MenuCommand> menuCommands = new ArrayList<>();
+        menuCommands.add(addProductMenu);
+        menuCommands.add(findAllProductMenu);
+        menuCommands.add(exitMenu);
+
+        UserMenu ui = new UserMenu(menuCommands);
+        ui.startUserMenu();
+
+    }
+}
