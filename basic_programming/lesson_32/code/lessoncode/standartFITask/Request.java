9d063da3cc7cc397c4b650c478bc2d0d35cf6d7e
StanislavPB <perminovsb@gmail.com>
2024-01-31 20:13:29 +0000

    add lesson 32

---
+++ basic_programming/lesson_32/code/lessoncode/standartFITask/Request.java
@@ -0,0 +1,27 @@
+package standartFITask;
+
+public class Request {
+    private String message;
+    private int number;
+
+    public Request(String message, int number) {
+        this.message = message;
+        this.number = number;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "message='" + message + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
