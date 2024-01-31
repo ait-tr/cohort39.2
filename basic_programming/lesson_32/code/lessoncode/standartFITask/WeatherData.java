88304f553582c5eed4d447f93a869c8fa12b32e4
StanislavPB <perminovsb@gmail.com>
2024-01-31 21:04:13 +0000

    add lesson 32

---
+++ basic_programming/lesson_32/code/lessoncode/standartFITask/WeatherData.java
@@ -0,0 +1,27 @@
+package standartFITask;
+
+class WeatherData {
+    private int temp;
+    private String description;
+
+    public WeatherData(int temperature, String description) {
+        this.temp = temperature;
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherData{" +
+                "temperature=" + temp +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
