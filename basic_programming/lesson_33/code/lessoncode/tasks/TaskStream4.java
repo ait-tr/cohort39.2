6b17b1d86f9e252f38a7a9beea3ceb944676f732
StanislavPB <perminovsb@gmail.com>
2024-02-02 20:32:54 +0000

    add lesson 33

---
+++ basic_programming/lesson_33/code/lessoncode/tasks/TaskStream4.java
@@ -0,0 +1,36 @@
+package tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TaskStream4 {
+    /*
+ создать коллекцию с номерами телефонов
+
+     */
+
+    public static void main(String[] args) {
+        List<Human> humans = new ArrayList<>();
+        humans.add(new Human("Ruslan", Arrays.asList("+26478384", "+4534632263")));
+        humans.add(new Human("Victor", Arrays.asList("+673473473", "+64584584567")));
+        humans.add(new Human("Petr", Arrays.asList("+25635623564", "+3263565465")));
+
+        List<String> phonesNumbers = new ArrayList<>();
+
+        for (Human human : humans){
+            phonesNumbers.addAll(human.getPhones());
+        }
+
+        System.out.println(phonesNumbers);
+
+        // с помощью Stream API
+
+        List<String> phones = humans.stream()
+                .flatMap(human -> human.getPhones().stream())
+                .toList();
+
+        System.out.println(phones);
+
+       }
+}
