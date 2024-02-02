7359e4aed2c219372ad25875e8c352a0e78f0329
StanislavPB <perminovsb@gmail.com>
2024-02-02 19:07:07 +0000

    add lesson 33

--- basic_programming/lesson_33/code/lessoncode/streamExample/Example1.java
+++ basic_programming/lesson_33/code/lessoncode/streamExample/Example1.java
@@ -3,6 +3,8 @@ package streamExample;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Example1 {
     public static void main(String[] args) {
@@ -34,6 +36,35 @@ public class Example1 {
                 .toList();
 
         System.out.println(result);
+
+        // ------------------ task 3 вариант 1 ----------------
+
+        List<Integer> numbers2 = Arrays.asList(2,3,4,5,2,4);
+
+        System.out.println(numbers2);
+
+        List<Integer> squareUniq = numbers2.stream()
+                .distinct()
+                .map(x -> x * x)
+                .toList();
+
+        System.out.println(squareUniq);
+
+        // ------------------ task 3 вариант 2 ----------------
+
+
+        Set<Integer> squareUniqSet = numbers2.stream()
+                .map(x -> x * x)
+                .collect(Collectors.toSet());
+
+        System.out.println(squareUniqSet);
+
+        List<Integer> listFromSet = squareUniqSet.stream()
+                .sorted()
+                .toList();
+
+        System.out.println(listFromSet);
+
 
 
 
