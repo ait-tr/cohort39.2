904278385722a12c2b62f80604f772fc33ac3cca
StanislavPB <perminovsb@gmail.com>
2024-02-05 18:21:39 +0000

    add lesson 34

--- basic_programming/lesson_34/code/lessoncode/studetnAndBook/Example5.java
+++ basic_programming/lesson_34/code/lessoncode/studetnAndBook/Example5.java
@@ -6,4 +6,19 @@ public class Example5 {
 задача вывести все прочитанные книги у всех студентов
 (в качестве дополнения, применить фильтр на любую тему)
  */
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Alex");
+        student1.addBook("Java 8 vs Java 21");
+        student1.addBook("Spring Boot in Action");
+        student1.addBook("Effective Java");
+
+        Student student2 = new Student("Thomas");
+        student1.addBook("HTML introducing");
+        student1.addBook("Effective Java");
+
+
+
+
+    }
 }
