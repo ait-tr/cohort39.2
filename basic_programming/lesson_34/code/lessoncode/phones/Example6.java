f111e040249d3270e0ca45831944abfa887a09c5
StanislavPB <perminovsb@gmail.com>
2024-02-05 18:57:30 +0000

    add lesson 34

---
+++ basic_programming/lesson_34/code/lessoncode/phones/Example6.java
@@ -0,0 +1,27 @@
+package phones;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Example6 {
+    public static void main(String[] args) {
+
+        List<Phone> phoneList = new ArrayList<>();
+        phoneList.add(new Phone("iPhone", 1000));
+        phoneList.add(new Phone("Samsung", 950));
+        phoneList.add(new Phone("Huawei", 750));
+        phoneList.add(new Phone("Motorola", 500));
+        phoneList.add(new Phone("Nokia", 620));
+        phoneList.add(new Phone("Xiaomi", 900));
+
+        Set<String> phones = phoneList.stream()
+                .filter(x -> x.getPrice() > 700)
+                .map(x -> x.getModel())
+                .collect(Collectors.toSet());
+
+        System.out.println(phones);
+
+    }
+}
