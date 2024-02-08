09d32758a073b4986c086e5c298ad6955f2f2863
StanislavPB <perminovsb@gmail.com>
2024-02-08 18:45:21 +0000

    add cons 24_05

---
+++ basic_programming/consultations/cons_24_05/bookReader/Book.java
@@ -0,0 +1,34 @@
+package bookReader;
+
+public class Book {
+    private String bookName;
+    private String author;
+    private int year;
+
+    public Book(String bookName, String author, int year) {
+        this.bookName = bookName;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
