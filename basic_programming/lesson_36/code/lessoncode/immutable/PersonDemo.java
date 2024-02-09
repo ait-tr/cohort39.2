3bb369778ea6a80382eb8cc01db535066ffe3b97
StanislavPB <perminovsb@gmail.com>
2024-02-09 21:03:49 +0000

    add lesson 36

---
+++ basic_programming/lesson_36/code/lessoncode/immutable/PersonDemo.java
@@ -0,0 +1,9 @@
+package immutable;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person person = new Person("John", 29);
+        System.out.println(person);
+
+    }
+}
---
+++ basic_programming/lesson_36/code/lessoncode/string/StringTimeTest.java
@@ -0,0 +1,27 @@
+package string;
+
+public class StringTimeTest {
+    public static void main(String[] args) {
+        String str = "";
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            str = str + i;
+        }
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Operations time is: " + (endTime - startTime));
+
+
+        StringBuilder sb = new StringBuilder();
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            sb.append(i);
+        }
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Operations time is: " + (endTime - startTime));
+
+    }
+}
