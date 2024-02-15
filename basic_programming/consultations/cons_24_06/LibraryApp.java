26c7b92101b6c30a07d1e4128b616d6de923b776
StanislavPB <perminovsb@gmail.com>
2024-02-15 20:24:01 +0000

    add cons_2024_06

--- basic_programming/consultations/cons_24_06/LibraryApp.java
+++ basic_programming/consultations/cons_24_06/LibraryApp.java
@@ -5,5 +5,11 @@ public class LibraryApp {
         LibraryService service = new LibraryService();
         service.readFromFile("basic_programming/consultations/cons_24_06/resources/books.txt");
         service.printAll();
+
+        System.out.println("------ BOOKS ---------");
+        service.printAllBooks();
+
+        System.out.println("------- MAGAZINE -------");
+        service.printAllMagazines();
     }
 }
