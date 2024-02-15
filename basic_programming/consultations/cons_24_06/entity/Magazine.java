8c4525f373d7cef4094ce5fa7f1eb9bb2a2d35c7
StanislavPB <perminovsb@gmail.com>
2024-02-15 19:38:33 +0000

    add cons_2024_06

--- basic_programming/consultations/cons_24_06/entity/Magazine.java
+++ basic_programming/consultations/cons_24_06/entity/Magazine.java
@@ -1,15 +1,37 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Magazine extends LibraryItem{
 
     private String issuedNumber;
+    private List<Article> articles;
 
-    public Magazine(String title, String author, int year, String issuedNumber) {
-        super(title, author, year);
+    public Magazine(String title, int year, String issuedNumber) {
+        super(title, year);
         this.issuedNumber = issuedNumber;
+        this.articles = new ArrayList<>();
+    }
+
+    public void addArticle(Article article){
+        articles.add(article);
     }
 
     public String getIssuedNumber() {
         return issuedNumber;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    @Override
+    public String toString() {
+        return "Magazine{" +
+                "issuedNumber='" + issuedNumber + '\'' +
+                ", articles=" + articles +
+                '}';
     }
 }
