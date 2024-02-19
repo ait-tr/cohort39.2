120e50eb7ecb3841c1862d6261e4c3493862f4ac
StanislavPB <perminovsb@gmail.com>
2024-02-19 19:14:55 +0000

    add lesson 40

---
+++ basic_programming/lesson_40/code/lessoncode/collections/list/arrayList/ArrayListDemo.java
@@ -0,0 +1,36 @@
+package collections.list.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+
+        List<String> strings2 = new ArrayList<>();
+
+
+        strings.add("1");
+        strings.add("2");
+        strings.add("3");
+        strings.add("4");
+        strings.add("5");
+        strings.add("6");
+        strings.add("7");
+        strings.add("8");
+        strings.add("9");
+        strings.add("10");
+        strings.add("11");
+
+        strings.trimToSize();
+        //strings2.trimToSize();
+
+
+
+
+
+
+
+    }
+}
---
+++ basic_programming/lesson_40/code/lessoncode/collections/list/linkedList/LinkedListArray.java
@@ -0,0 +1,20 @@
+package collections.list.linkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListArray {
+    public static void main(String[] args) {
+        LinkedList<String> strings = new LinkedList<>();
+
+        strings.add("text1");
+        strings.add("text2");
+        strings.add("text3");
+        strings.add("text4");
+        strings.add("text5");
+        strings.add("text6");
+        strings.add("text7");
+        strings.add("text8");
+
+
+    }
+}
